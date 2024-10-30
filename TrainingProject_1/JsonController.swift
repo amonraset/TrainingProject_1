7f22abfc549a8adefa965863f47b726b3705385e
amonraset <minia777@icloud.com>
2024-10-30 21:46:28 +0300

    s

--- TrainingProject_1/JsonController.swift
+++ TrainingProject_1/JsonController.swift
@@ -9,20 +9,37 @@ import UIKit
 
 class JsonController: UIViewController {
     
+    struct SpaceInfo: Decodable {
+        let message: String
+        let number: Int
+    }
     
     
     @IBOutlet weak var JsonText: UITextView!
-    
     @IBAction func loadjson(_ sender: Any) {
         self.JsonText.text = "whats up"
     }
     
-  
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    
+    
+    func loadJson(){
+        let stringUrl = "http://open-notify.org/Open-Notify-API/People-In-Space/"
+        guard let url = URL(string: stringUrl) else { return}
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard error == nil else {
+                print(error?.localizedDescription ?? "noDesc")
+            return }
+            guard let data = data else { return }
+            guard let spaceInfo = try? JSONDecoder().decode(SpaceInfo.self, from: data) else { print ("Error = cant parse Space Info")
+                return
+            }
+            let y = JsonController.SpaceInfo.init(message: <#T##String#>, number: <#T##Int#>)
+        }
         
     }
+    
 }
-
